a0b8506bf5c399bcdc2dcc5b974c48eac9376328
huberrtooo <hubert@helloyou.online>
2023-04-03 22:51:55 +0200

    hero section done on desktop

--- wp-content/themes/develtioworktheme/front-page.php
+++ wp-content/themes/develtioworktheme/front-page.php
@@ -1,9 +1,25 @@
 <?php get_header(); ?>
 
-<div class="container-fluid">
-    <div class="container">
-        <div class="row">
-            <div class="col">
+<div class="container-fluid py-4 hero" style="background-image: url('<?php echo esc_url(get_field('hero_bg')['url']); ?>');">
+    <div class="container hero__form">
+        <div class="row py-5 d-flex justify-content-end">
+            <div class="col-lg-5">
+                <div class="hero__form-box p-5">
+                    <p class="hero__form-header"><?php the_field('hero_form-header'); ?></p>
+                    <div class="hero__contact-form">
+                        <?php echo(do_shortcode('[contact-form-7 id="82" title="Formularz"]')); ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid py-5 hero__bluebox">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-7">
+                    <h1 class="hero__header"><?php the_field('hero_header'); ?></h1>
+                    <h2 class="hero__desc"><?php the_field('hero_desc'); ?></h2>
+                </div>
             </div>
         </div>
     </div>
--- wp-content/themes/develtioworktheme/header.php
+++ wp-content/themes/develtioworktheme/header.php
@@ -20,8 +20,7 @@
             <button class="navbar-toggler menu__button" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <svg xmlns="http://www.w3.org/2000/svg" width="21.885" height="19.067" viewBox="0 0 21.885 19.067"><path d="M0,0V2.736H21.885V0ZM0,8.125V10.86H21.885V8.125Zm0,8.207v2.736H21.885V16.332Z" fill="#000"/></svg>            </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <?php
-                wp_nav_menu(array(
+                <?php wp_nav_menu(array(
                     'theme_location' => 'header-menu',
                     'container' => true,
                     'menu_class' => '',
@@ -29,8 +28,7 @@
                     'items_wrap' => '<ul id="%1$s" class="navbar-nav me-auto w-100 justify-content-between text-center align-items-center %2$s">%3$s</ul>',
                     'depth' => 1,
                     'walker' => new bootstrap_5_wp_nav_menu_walker()
-                ));
-                ?>
+                )); ?>
             </div>
         </div>
     </nav>
